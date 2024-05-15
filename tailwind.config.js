1861d4e3c32bd90e27fecb0b3a3d7b87057b556e
dipishBisht <dipish1718@gmail.com>
2024-05-15 11:16:51 +0530

    Styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        topToBottom: {
+          "from": { "backgroundPosition":"top" },
+          "to": { "backgroundPosition":"bottom" },
+        }
+      }
+    },
   },
   plugins: [],
 }
-
