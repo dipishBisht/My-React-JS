adabd11ccb738ad5e57eb609e624e7cb6f3bcefc
dipishBisht <dipish1718@gmail.com>
2024-06-03 12:00:55 +0530

    additional styling layer base

--- src/Practise Files/JSX Files/Netflix.jsx
+++ src/Practise Files/JSX Files/Netflix.jsx
@@ -1,16 +1,9 @@
+import React from 'react'
 
 const Netflix = () => {
-
-    const API = {
-        method: 'GET',
-        headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZTIyOGM0MmUzNjkyOTM2M2MwYWQ0YzYyODZhZTk4MiIsInN1YiI6IjY2NTQ0MWZlNTU5ZWYxYmM3NDVhOTljYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XB2R8NcT8izsMrZgF-M-gfsJ8aRQ1MvWaCkXiz9_2J8'
-        }
-    };
-    return (
-        <div>
-        
-        </div>
-    );
+  return (
+    <div>Netflix</div>
+  )
 }
+
+export default Netflix
