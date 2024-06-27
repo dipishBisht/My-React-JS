3050f9b122da6fb189e7a94d6990343d181baa4b
dipishBisht <dipish1718@gmail.com>
2024-06-27 13:43:08 +0530

    temp file

--- src/Practise Files/JSX Files/new.jsx
+++ src/Practise Files/JSX Files/new.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
 
-const headingElement = <h1>Heading Element</h1>
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const AnotherHeading = () => <h1>Another Heading Component</h1>
 
-const HeadingComponent = () => (
-    <div>
-        <AnotherHeading />
-        <h1>Heading Component</h1>
-        {headingElement}
-        {AnotherHeading()}
-    </div>
-);
 
-root.render(<HeadingComponent />);
+const Main = () => {
+
+    useEffect(() => {
+        getData();
+    })
+
+    async function getData() {
+        console.log("Hello World");
+    }
+
+    return (
+        <div>
+            xsadd
+        </div>
+    );
+}
+
+root.render(<Main />);
